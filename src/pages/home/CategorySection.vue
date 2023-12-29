46756f7d7a4b8d9b7f90dedf9fe945e6da16c3df
Abraham Bartoloni <116020038+Bartoloni00@users.noreply.github.com>
2023-12-29 09:46:10 -0300

    Hot fix
    me lo recomendo @Ezearevalodev
    
    Co-Authored-By: Ezequiel Arevalo <111710833+Ezearevalodev@users.noreply.github.com>

--- src/pages/home/CategorySection.vue
+++ src/pages/home/CategorySection.vue
@@ -2,67 +2,6 @@
 
 </script>
 <template>
-    <!-- <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-rows-4 gap-3 m-auto md:grid-rows-none">
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg"
-            :class="`row-span-4`"
-        >
-            <img 
-                src="/imgs/DreamShaper_v7_a_banner_for_a_videogame_1.jpg" 
-                alt="" 
-                class="w-full h-full object-cover rounded-lg">
-        </div>
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg"
-            :class="`row-span-2 col-start-3 row-start-1`"
-        >
-            <img src="/gif/OPThalflife.gif" class="w-full h-full object-cover rounded-lg"/>
-        </div>
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg flex justify-center items-center h-full"
-            :class="`row-span-4 col-start-4 row-start-1`"
-        >
-            <p class="m-4 text-[18px]">
-                Tu destino definitivo para la mejor experiencia de compra de videojuegos en línea! Sumérgete en un mundo de emociones, aventuras y diversión ilimitada con nuestra amplia selección de títulos de todos los géneros. Desde épicas sagas de rol hasta trepidantes juegos de disparos, tenemos algo para todos los gustos.
-            </p>
-        </div>
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg"
-            :class="`col-span-2 row-span-4 col-start-2 row-start-3`"
-        >
-            <div class="flex justify-center items-center h-full">
-                <h2 class="text-4xl">
-                Cuevana Games Categorias
-                </h2>
-            </div>
-        </div>
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg"
-            :class="`row-span-2 col-start-1 row-start-5`"
-        >
-            <img src="/gif/OPTportal.gif" class="w-full h-full object-cover rounded-lg"/>
-        </div>
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg"
-            :class="`row-span-4 col-start-4 row-start-5`"
-        >
-            <img 
-                src="/imgs/DreamShaper_v7_a_banner_for_a_videogame_3.jpg" 
-                alt="" 
-                class="w-full h-full object-cover rounded-lg">
-        </div>
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg flex justify-center items-center h-full"
-            :class="`col-span-2 row-span-2 col-start-1 row-start-7`"
-        >
-            <p class="m-4 text-[18px]">
-            Explora nuestro catálogo curado con cuidado y encuentra joyas ocultas que harán que cada sesión de juego sea única. Además, no te pierdas nuestras ofertas irresistibles y descuentos exclusivos que harán que tu billetera sonría.
-            </p>
-        </div>
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg"
-            :class="`row-span-2 col-start-3 row-start-7`"
-        >
-            <img src="/gif/OPTminecraft.gif" class="w-full h-full object-cover rounded-lg"/>
-        </div>
-        <div class="shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg"
-            :class="`row-span-2 col-start-2 row-start-1`"
-        >
-            <img src="/gif/OPTfiveNightsAtFreddys.gif" class="w-full h-full object-cover rounded-lg"/>
-        </div>
-    </div> -->
     <div class="grid lg:grid-cols-4 lg:grid-rows-4 gap-3 m-auto grid-cols-2 grid-rows-7">
         <!-- 1 -->
         <div class="h-[300px] shadow-lg hover:shadow-xl hover:bg-slate-200 transition-all duration-300 rounded-lg"
